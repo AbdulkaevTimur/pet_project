f37426f8f5ec82b97bc6eb27d77938b1b65e30e0
AbdulkaevTimur <badun.ep@icloud.com>
2025-02-03 00:23:41 +0400

    CRUD

--- main.go
+++ main.go
@@ -6,12 +6,19 @@ import (
 	"github.com/gorilla/mux"
 	"io"
 	"net/http"
+	"strconv"
 )
 
-func getMessages(w http.ResponseWriter, r *http.Request) {
+var messages []Message
+
+func GetMessages(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
-		var messages []Message
-		DB.Find(&messages)
+		if err := DB.Find(&messages).Error; err != nil {
+			fmt.Fprintln(w, Response{
+				Status:  "Error",
+				Message: "Could not get message",
+			})
+		}
 		json.Marshal(messages)
 		fmt.Fprintln(w, messages)
 	} else {
@@ -19,19 +26,83 @@ func getMessages(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func createMessage(w http.ResponseWriter, r *http.Request) {
+func CreateMessage(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
-		data := Message{}
+		message := Message{}
 		body, err := io.ReadAll(r.Body)
 		if err != nil {
-			fmt.Fprintln(w, "Ошибка чтения данных")
+			fmt.Fprintln(w, Response{
+				Status:  "Error",
+				Message: "Could not read body",
+			})
 			return
 		}
-		json.Unmarshal(body, &data)
-		DB.Create(&data)
-		fmt.Fprintln(w, data)
+		json.Unmarshal(body, &message)
+		DB.Create(&message)
+		fmt.Fprintln(w, message)
 	} else {
 		fmt.Fprintln(w, "Поддерживается только метод POST")
+	}
+}
+
+func UpdateMessage(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPatch {
+		var updatedMessage Message
+		idParam := mux.Vars(r)["id"]
+		id, err := strconv.Atoi(idParam)
+		if err != nil {
+			fmt.Fprintln(w, Response{
+				Status:  "Error",
+				Message: "Bad ID",
+			})
+		}
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			fmt.Fprintln(w, Response{
+				Status:  "Error",
+				Message: "Could not read body",
+			})
+			return
+		}
+		json.Unmarshal(body, &updatedMessage)
+		if err := DB.Model(&Message{}).Where("id = ?", id).Updates(map[string]interface{}{
+			"task":    updatedMessage.Task,
+			"is_done": updatedMessage.IsDone,
+		}).Error; err != nil {
+			fmt.Fprintln(w, Response{
+				Status:  "Error",
+				Message: "Could not update message",
+			})
+		} else {
+			fmt.Fprintln(w, Response{
+				Status:  "Success",
+				Message: "Message updated",
+			})
+		}
+	}
+}
+
+func DeleteMessage(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodDelete {
+		idParam := mux.Vars(r)["id"]
+		id, err := strconv.Atoi(idParam)
+		if err != nil {
+			fmt.Fprintln(w, Response{
+				Status:  "Error",
+				Message: "Bad ID",
+			})
+		}
+		if err := DB.Delete(&Message{}, id).Error; err != nil {
+			fmt.Fprintln(w, Response{
+				Status:  "Error",
+				Message: "Could not delete message",
+			})
+		} else {
+			fmt.Fprintln(w, Response{
+				Status:  "Success",
+				Message: "Message deleted",
+			})
+		}
 	}
 }
 
@@ -41,7 +112,9 @@ func main() {
 	DB.AutoMigrate(&Message{})
 
 	router := mux.NewRouter()
-	router.HandleFunc("/api/messages", createMessage).Methods("POST")
-	router.HandleFunc("/api/messages", getMessages).Methods("GET")
+	router.HandleFunc("/api/messages", CreateMessage).Methods("POST")
+	router.HandleFunc("/api/messages", GetMessages).Methods("GET")
+	router.HandleFunc("/api/messages/{id}", UpdateMessage).Methods("PATCH")
+	router.HandleFunc("/api/messages/{id}", DeleteMessage).Methods("DELETE")
 	http.ListenAndServe(":8080", router)
 }
--- orm.go
+++ orm.go
@@ -7,3 +7,8 @@ type Message struct {
 	Task   string `json:"task"`
 	IsDone bool   `json:"is_done"`
 }
+
+type Response struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
