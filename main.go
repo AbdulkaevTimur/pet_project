042a2e44471f65bff0e4dfa57f46dcfc497897e6
AbdulkaevTimur <badun.ep@icloud.com>
2025-02-02 14:07:50 +0400

    Database

--- main.go
+++ main.go
@@ -3,37 +3,45 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
+	"io"
 	"net/http"
 )
 
-var task, message string
-
-type requestBody struct {
-	Message string `json:"message"`
-}
-
-func GetHandler(w http.ResponseWriter, r *http.Request) {
+func getMessages(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
-		fmt.Fprintln(w, message)
+		var messages []Message
+		DB.Find(&messages)
+		json.Marshal(messages)
+		fmt.Fprintln(w, messages)
 	} else {
 		fmt.Fprintln(w, "Поддерживается только метод GET")
 	}
 }
 
-func PostHandler(w http.ResponseWriter, r *http.Request) {
+func createMessage(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
-		data := requestBody{}
-		decoder := json.NewDecoder(r.Body)
-		decoder.Decode(&data)
-		task = data.Message
-		message = "hello, " + task
+		data := Message{}
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			fmt.Fprintln(w, "Ошибка чтения данных")
+			return
+		}
+		json.Unmarshal(body, &data)
+		DB.Create(&data)
+		fmt.Fprintln(w, data)
 	} else {
 		fmt.Fprintln(w, "Поддерживается только метод POST")
 	}
 }
 
 func main() {
-	http.HandleFunc("/get", GetHandler)
-	http.HandleFunc("/post", PostHandler)
-	http.ListenAndServe("localhost:8080", nil)
+	InitDB()
+
+	DB.AutoMigrate(&Message{})
+
+	router := mux.NewRouter()
+	router.HandleFunc("/api/messages", createMessage).Methods("POST")
+	router.HandleFunc("/api/messages", getMessages).Methods("GET")
+	http.ListenAndServe(":8080", router)
 }
